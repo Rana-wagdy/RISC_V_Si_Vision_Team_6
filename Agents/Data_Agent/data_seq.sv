205985059f35dd0af9e910ac5a9e9e2a3af31289
Rana-wagdy <73410986+Rana-wagdy@users.noreply.github.com>
2025-05-05 21:31:03 +0300

    Update data_seq.sv

--- Agents/Data_Agent/data_seq.sv
+++ Agents/Data_Agent/data_seq.sv
@@ -1,7 +1,7 @@
 class data_seq extends uvm_sequence #(data_seq_item);
     `uvm_object_utils(data_seq)    
-    ins_seq_item req,rsp;
-    ins_seq_item q [$:2];
+    data_seq_item req,rsp;
+    data_seq_item q [$:2];
     [7:0] data_mem [int];
     //[31:0] data_mem [32];
 
@@ -14,9 +14,9 @@ class data_seq extends uvm_sequence #(data_seq_item);
 
     virtual task pre_body();
         `uvm_info("Seq", "Starting pre body ", UVM_MEDIUM)
-        cfg=alu_cfg::type_id::create("cfg");
+        cfg=data_agent_cfg::type_id::create("cfg");
 
-        if(!uvm_config_db#(alu_cfg)::get(m_sequencer, "", "cfg", cfg))
+        if(!uvm_config_db#(data_agent_cfg)::get(m_sequencer, "", "cfg", cfg))
             `uvm_fatal("NOCFG", "No configuration object found");
         
         num_items = cfg.num_items;
@@ -28,10 +28,10 @@ class data_seq extends uvm_sequence #(data_seq_item);
 
         `uvm_info("Seq", "Starting sequence", UVM_MEDIUM)
     
-        item = seq_item::type_id::create("item");
+        item = data_seq_item::type_id::create("item");
         // Load the memory with some important values
         {data_mem[0],data_mem[1],data_mem[2],data_mem[3]} = 32'b0;               // zero
-        {data_mem[4],data_mem[5],data_mem[6],data_mem[7]} = {1'b0,31{1'b1}};     // max positive
+        {data_mem[4],data_mem[5],data_mem[6],data_mem[7]} = {1'b0,31{1'b1}};     // max positive%%%?
         {data_mem[8],data_mem[8],data_mem[10],data_mem[11]} = {1'b1,31{1'b0}};   // max positive
 
 
@@ -43,28 +43,26 @@ class data_seq extends uvm_sequence #(data_seq_item);
             
             start_item(rsp);
                 rsp.copy(req);
-                rsp.data_rdata_i = 0;
+                rsp.rdata = 0;
 
-                if(req.data_we_o) begin
-                    foreach(req.data_be_o[i]) begin
-                        if(req.data_be_o[i]) begin
-                            data_mem[req.data_addr_o+i] = req.data_wdata_o>>(i*8);
+                if(req.we) begin
+                    foreach(req.be[i]) begin
+                        if(req.be[i]) begin
+                            data_mem[req.addr+i] = req.wdata>>(i*8);
                         end
                     end
 
                 end
                 else begin
 
-                    foreach(req.data_be_o[i]) begin
-                        if(req.data_be_o[i]) begin
-                            rsp.data_rdata_i |= data_mem[req.data_addr_o+i]<<(i*8);
+                    foreach(req.be[i]) begin
+                        if(req.be[i]) begin
+                            rsp.rdata |= data_mem[req.addr+i]<<(i*8);
                         end
                     end
                 end
             finish_item(rsp);
-            
-
-
+        
         end
         
 
